eac5aa2c56b4a8a47f459e68bb4284959eae6dc5
Nocktiss <anthony.grupposo@gmail.com>
2020-08-12 15:53:08 +0200

    Updates

---
+++ precache-manifest.649a67b38153e404edb6b3a4ed69f4b9.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c44cf283ebb029b1d6d81f88377837",
+    "url": "/Nocktiss/index.html"
+  },
+  {
+    "revision": "6a90770594bd3c2e96fb",
+    "url": "/Nocktiss/static/css/2.49217409.chunk.css"
+  },
+  {
+    "revision": "1d05848b0dc03585de8a",
+    "url": "/Nocktiss/static/css/main.edd33452.chunk.css"
+  },
+  {
+    "revision": "6a90770594bd3c2e96fb",
+    "url": "/Nocktiss/static/js/2.396b0200.chunk.js"
+  },
+  {
+    "revision": "a7865a9a55add35a1fe5f9fbe7ce695f",
+    "url": "/Nocktiss/static/js/2.396b0200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d05848b0dc03585de8a",
+    "url": "/Nocktiss/static/js/main.a90e5da2.chunk.js"
+  },
+  {
+    "revision": "b517f5265e86fdf748fd",
+    "url": "/Nocktiss/static/js/runtime-main.a9981ca4.js"
+  },
+  {
+    "revision": "911a1ba44a6288c70478ba6d727ba870",
+    "url": "/Nocktiss/static/media/arrowLeftwhite.911a1ba4.svg"
+  },
+  {
+    "revision": "066a4f5b598eb7871bac1f124c942ccb",
+    "url": "/Nocktiss/static/media/assietteFermentee.066a4f5b.jpg"
+  },
+  {
+    "revision": "6a69f6013417f2859416e966b8ab1241",
+    "url": "/Nocktiss/static/media/assietteMixte.6a69f601.jpg"
+  },
+  {
+    "revision": "a54ecc29747978b70f18042d7deef980",
+    "url": "/Nocktiss/static/media/bierreMainDegrade.a54ecc29.png"
+  },
+  {
+    "revision": "53be22f14b88490a2f48aa251171670b",
+    "url": "/Nocktiss/static/media/brableSolo.53be22f1.png"
+  },
+  {
+    "revision": "bfa13d322086e0b85dc506e6a9a87eb3",
+    "url": "/Nocktiss/static/media/brownAle.bfa13d32.png"
+  },
+  {
+    "revision": "0758e82ca2b1e83554957d9ad4ea411d",
+    "url": "/Nocktiss/static/media/cassisAuLevain.0758e82c.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/Nocktiss/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/Nocktiss/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/Nocktiss/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/Nocktiss/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/Nocktiss/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/Nocktiss/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/Nocktiss/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/Nocktiss/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/Nocktiss/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/Nocktiss/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/Nocktiss/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/Nocktiss/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/Nocktiss/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/Nocktiss/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/Nocktiss/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "92820bd56bb968f1288f2df76b000d74",
+    "url": "/Nocktiss/static/media/fusoJaune.92820bd5.png"
+  },
+  {
+    "revision": "56b60a9ef69f0f814e96b2ae72dc0018",
+    "url": "/Nocktiss/static/media/fusoOrange.56b60a9e.png"
+  },
+  {
+    "revision": "fc73d52b9958cbf5d0cceb7187413b2c",
+    "url": "/Nocktiss/static/media/fusoRouge.fc73d52b.png"
+  },
+  {
+    "revision": "1798c726297cb700f8f726d3ceb37b3b",
+    "url": "/Nocktiss/static/media/houblonCommeLesBles.1798c726.png"
+  },
+  {
+    "revision": "3ba94136f9627f33b28c345fcd3ef320",
+    "url": "/Nocktiss/static/media/houmousMoutabal.3ba94136.jpeg"
+  },
+  {
+    "revision": "ceec61ea8ef06db09a48eaf4dc018ac5",
+    "url": "/Nocktiss/static/media/hypePa.ceec61ea.jpg"
+  },
+  {
+    "revision": "bc89e88ffa7830fc0c154984dead1019",
+    "url": "/Nocktiss/static/media/kefirCitron.bc89e88f.jpg"
+  },
+  {
+    "revision": "e3e16994e556eb09674e55a5c01f93a6",
+    "url": "/Nocktiss/static/media/kombucha.e3e16994.jpg"
+  },
+  {
+    "revision": "d097cdcd8eec9a5fcccc1f1765c50c97",
+    "url": "/Nocktiss/static/media/logoBrable.d097cdcd.png"
+  },
+  {
+    "revision": "b21ec159c4c871401593dc42f1911f08",
+    "url": "/Nocktiss/static/media/perHugo.b21ec159.jpg"
+  },
+  {
+    "revision": "2bc24188c1fcf392470ed48409ca0310",
+    "url": "/Nocktiss/static/media/plancheDeFromage.2bc24188.jpg"
+  },
+  {
+    "revision": "acbeee7035e0ef18cb01c31af4793f99",
+    "url": "/Nocktiss/static/media/saucisson.acbeee70.jpeg"
+  },
+  {
+    "revision": "8b1bb88c622ba5bc252529868089c6ad",
+    "url": "/Nocktiss/static/media/sessionIpa.8b1bb88c.jpg"
+  },
+  {
+    "revision": "4702035f4f616adb17e8ee198a7e5b4c",
+    "url": "/Nocktiss/static/media/sourkefir.4702035f.jpg"
+  },
+  {
+    "revision": "e2a0b597b9494f5a6fc8b59513578c24",
+    "url": "/Nocktiss/static/media/tartinadesDApero.e2a0b597.jpg"
+  },
+  {
+    "revision": "ab77f3968988bc8ad74ee87c3f2316e2",
+    "url": "/Nocktiss/static/media/tchin.ab77f396.gif"
+  },
+  {
+    "revision": "2e2cad19c23737d70c5ad33e5d4db1c3",
+    "url": "/Nocktiss/static/media/trash.2e2cad19.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BeerBrableV1/precache-manifest.6b999533c02f80c3eea963e6bb0b446f.js"
+  "/Nocktiss/precache-manifest.649a67b38153e404edb6b3a4ed69f4b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BeerBrableV1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nocktiss/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
